a4f9866070c3c539386d5e1169a7867d0863250a
607112663 <rudrajyoti.biswas@bt.com>
2019-04-22 21:57:47 +0530

    tech gig 2019 round 1 - new year party - failed for time

--- src/main/java/ds/TechGigNewYarPartyMain.java
+++ src/main/java/ds/TechGigNewYarPartyMain.java
@@ -8,26 +8,23 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class TechGigNewYarPartyMain
-{
+public class TechGigNewYarPartyMain {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int numberOfTestCases = Integer.parseInt(bufferedReader.readLine());
         StringBuilder finalResultBuilder = new StringBuilder();
-        for (int i=0;i<numberOfTestCases;i++)
-        {
+        for (int i = 0; i < numberOfTestCases; i++) {
             bufferedReader.readLine();
             String tickets = bufferedReader.readLine();
             int[] numericTicketValues = Arrays.stream(tickets.split(" "))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-            TechGigNewYarParty techGigNewYarParty=new TechGigNewYarParty(numericTicketValues);
-            if (i!=0)
-            {
+            TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(numericTicketValues);
+            if (i != 0) {
                 finalResultBuilder.append("\n");
             }
-            finalResultBuilder.append(techGigNewYarParty.findMaxSum().consolidateReverseWay());
+            finalResultBuilder.append(techGigNewYearParty.findMaxSum().consolidateReverseWay());
         }
         System.out.println(finalResultBuilder.toString());
     }
@@ -36,12 +33,12 @@ public class TechGigNewYarPartyMain
 
 
 @SuppressWarnings("WeakerAccess")
-class TechGigNewYarParty {
+class TechGigNewYearParty {
 
     private final int[] input;
     private List<IndividualBestCase> individualBestCases = new ArrayList<>();
 
-    public TechGigNewYarParty(int[] input) {
+    public TechGigNewYearParty(int[] input) {
         this.input = input;
     }
 
@@ -75,38 +72,102 @@ class TechGigNewYarParty {
             individualBestCases.add(new IndividualBestCase(anInput));
         }
 
+        individualBestCases.get(1).updateSelfFromTrainingElement(null, individualBestCases.get(0));
 
         for (int i = 2; i < input.length; i++) {
             individualBestCases.get(i).updateSelfFromTrainingElement(individualBestCases.get(i - 2), individualBestCases.get(i - 1));
         }
 
-        IndividualBestCase max = individualBestCases
+
+        Trail max = individualBestCases
+                .get(individualBestCases.size() - 1)
+                .getTrails()
                 .stream()
-                .max((o1, o2) -> {
-                    if (o1.getMaxSum()!=o2.getMaxSum())
-                        return o1.getMaxSum()-o2.getMaxSum();
-                    return o1.getTrail().get(o1.getTrail().size()-1) - o2.getTrail().get(o2.getTrail().size()-1);
-                })
+                .max(Trail::compareTo)
                 .orElse(null);
 
         assert max != null;
-        return new Result(max.getTrail());
+        return new Result(max.getTrailElements());
     }
 }
+
+class Trail implements Comparable {
+    private final List<Integer> trailElements;
+
+
+    Trail(int firstElement) {
+        this.trailElements = new ArrayList<Integer>() {
+            {
+                add(firstElement);
+            }
+        };
+    }
+
+    public void deleteAllElements() {
+        trailElements.clear();
+    }
+
+    public List<Integer> getTrailElements() {
+        return trailElements;
+    }
+
+    public int getLastElement() {
+        return trailElements.get(trailElements.size() - 1);
+    }
+
+    public void addToTrail(int element) {
+        this.trailElements.add(element);
+    }
+
+    public int sumUp() {
+        return trailElements.stream().reduce((integer, integer2) -> integer + integer2).orElse(0);
+    }
+
+    public Trail copy() {
+        Trail copy = new Trail(0);
+        copy.deleteAllElements();
+        trailElements.forEach(integer -> copy.trailElements.add(integer));
+        return copy;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Trail otherTrail = (Trail) o;
+        if (this.sumUp() != otherTrail.sumUp())
+            return this.sumUp() - otherTrail.sumUp();
+        int trailLength = this.trailElements.size();
+        int otherTrailLength = otherTrail.getTrailElements().size();
+        int shorterLength = trailLength >= otherTrailLength ? otherTrailLength : trailLength;
+        for (int i = shorterLength - 1; i >= 0; i--) {
+            if (trailElements.get(i).intValue() != otherTrail.getTrailElements().get(i).intValue()) {
+                return trailElements.get(i).intValue() - otherTrail.getTrailElements().get(i).intValue();
+            }
+        }
+        if (trailLength > otherTrailLength) {
+            return -1;
+        } else if (otherTrailLength > trailLength) {
+            return 1;
+        }
+        return 0;
+    }
+
+
+}
+
 
 @SuppressWarnings("WeakerAccess")
 class IndividualBestCase {
     private final int baseValue;
-    private final List<Integer> trail;
+    private final List<Trail> trails;
     private int maxSum;
 
 
     IndividualBestCase(int baseValue) {
         this.baseValue = baseValue;
         this.maxSum = baseValue;
-        this.trail = new ArrayList<Integer>() {
+        this.trails = new ArrayList<Trail>() {
             {
-                add(baseValue);
+                add(new Trail(baseValue));
             }
         };
     }
@@ -115,23 +176,52 @@ class IndividualBestCase {
         return baseValue;
     }
 
+    public void clearTrails() {
+        trails.clear();
+    }
+
     public int getMaxSum() {
         return maxSum;
     }
 
-    public List<Integer> getTrail() {
-        return trail;
+    public List<Trail> getTrails() {
+        return trails;
+    }
+
+    public List<Trail> copyTrails() {
+        List<Trail> copy = new ArrayList<>();
+        trails.forEach(trail -> copy.add(trail.copy()));
+        return copy;
     }
 
     public void updateSelfFromTrainingElement(IndividualBestCase oneBeforeAdjacentTrailingElement, IndividualBestCase adjacentTrailingElement) {
+        if (oneBeforeAdjacentTrailingElement == null) {
+            if (adjacentTrailingElement.getMaxSum() > this.getBaseValue()) {
+                this.maxSum = adjacentTrailingElement.getMaxSum();
+                this.clearTrails();
+                this.trails.addAll(adjacentTrailingElement.copyTrails());
+            }
+            return;
+        }
         int valueWithOneBeforeAdjacent = oneBeforeAdjacentTrailingElement.getMaxSum() + this.getBaseValue();
+        if ((adjacentTrailingElement.getMaxSum() == valueWithOneBeforeAdjacent) && (valueWithOneBeforeAdjacent >= baseValue)) {
+            this.clearTrails();
+            this.trails.addAll(oneBeforeAdjacentTrailingElement.copyTrails());
+            this.trails.forEach(trail -> trail.getTrailElements().add(baseValue));
+            this.trails.addAll(adjacentTrailingElement.copyTrails());
+
+            this.maxSum = valueWithOneBeforeAdjacent;
+            return;
+        }
         if (adjacentTrailingElement.getMaxSum() > valueWithOneBeforeAdjacent) {
             this.maxSum = adjacentTrailingElement.getMaxSum();
-            this.trail.remove(0);
-            this.trail.addAll(0, adjacentTrailingElement.getTrail());
+            this.clearTrails();
+            this.trails.addAll(adjacentTrailingElement.copyTrails());
         } else if (valueWithOneBeforeAdjacent > this.getBaseValue()) {
             this.maxSum = valueWithOneBeforeAdjacent;
-            this.trail.addAll(0, oneBeforeAdjacentTrailingElement.getTrail());
+            this.clearTrails();
+            this.trails.addAll(oneBeforeAdjacentTrailingElement.copyTrails());
+            this.trails.forEach(trail -> trail.getTrailElements().add(baseValue));
         }
     }
 
@@ -150,8 +240,7 @@ class Result {
         StringBuilder resultBuffer = new StringBuilder();
         Collections.reverse(trail);
         trail.forEach(integer -> {
-            if (!integer.equals(0))
-            {
+            if (!integer.equals(0)) {
                 resultBuffer.append(integer);
             }
         });
---
+++ src/test/java/ds/TechGigNewYearPartyTest.java
@@ -0,0 +1,190 @@
+package ds;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class TechGigNewYearPartyTest {
+
+    @Test
+    public void testCase1() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {6,5,1,-9,-8,4,-1,11});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("11416") );
+    }
+
+    @Test
+    public void testCase2() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {-1,7,8,-5,4});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("48") );
+    }
+
+    @Test
+    public void testCase3() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {11,12,-2,-1});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("12") );
+    }
+
+    @Test
+    public void testCase4() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {4,3,4,5});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("54") );
+    }
+
+    @Test
+    public void testCase5() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {5, 10, 4, -1});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("10") );
+    }
+
+    @Test
+    public void testCase6() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {-1, -1, 10, 1});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("10") );
+    }
+
+    @Test
+    public void testCase7() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {6, 9, -3, 9, 9, 2, 4, 8, -7, -5, 9});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("98299") );
+    }
+
+    @Test
+    public void testCase8() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {9,-9});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("9") );
+    }
+
+    @Test
+    public void testCase9() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {1,2,4,0,0,0,7,9,0,0,0,0,-1,-2,-3,100});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("100941") );
+    }
+
+    @Test
+    public void testCase10() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {4, 5, 4, 3, -2, -2, -2, 4, 5, 4, 3});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("4444") );
+    }
+
+    @Test
+    public void testCase11() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {9,1,2,8});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("89") );
+    }
+
+    @Test
+    public void testCase12() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {-1,-2,-3,-4});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("-1") );
+    }
+
+    @Test
+    public void testCase13() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {2,4,6,4});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("62") );
+    }
+
+    @Test
+    public void testCase14() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {4,6,4,2});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("44") );
+    }
+
+    @Test
+    public void testCase15() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {5,6,3,2});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("35") );
+    }
+
+    @Test
+    public void testCase16() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {5,5,4,4});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("45") );
+    }
+
+    @Test
+    public void testCase17() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {6,0,0,2});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("26") );
+    }
+
+    @Test
+    public void testCase18() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {5,4,2,4,1});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("45") );
+    }
+
+    @Test
+    public void testCase19() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {1,4,2,4,5});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("54") );
+    }
+
+    @Test
+    public void testCase20() {
+        TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {-1,-1,0,-1});
+        Result maxSum = techGigNewYearParty.findMaxSum();
+        assertThat(maxSum.consolidateReverseWay(), is("") );
+    }
+
+    @Test
+    public void testCase21()
+    {
+        List<String> expectedTestCases = new ArrayList<String>()
+        {{
+            add("4 5 4 3 1 7 9 3 1,19144");
+            add("8 9 -2 3 5 6 -6 -2 1 0,1639");
+            add("48 65 184 24 17 108 56 423 148 44,4442310818448");
+            add("4 5 4 3 -2 -2 -2 4 5 4 3,4444");
+            add("12 15 25 -15 35,352512");
+            add("1 2 50 3 2 50,50501");
+            add("5 4 6 8,85");
+            add("9 1 2 8 5 4 6 8,8489");
+            add("4 7 4 1 1 5,544");
+            add("3 7 5 1 1 5,553");
+            add("1 2 2 1 2 2,221");
+        }};
+
+
+        expectedTestCases.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                String input = s.split(",")[0];
+                String output = s.split(",")[1];
+
+
+                TechGigNewYearParty techGigNewYearParty=new TechGigNewYearParty(Arrays.stream(input.split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray());
+                assertThat(techGigNewYearParty.findMaxSum().consolidateReverseWay(), is(output));
+
+            }
+        });
+
+    }
+}
