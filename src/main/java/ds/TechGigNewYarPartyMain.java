f004c88742f24e274f20d28c26ed020091bcf38f
607112663 <rudrajyoti.biswas@bt.com>
2019-04-29 11:37:34 +0530

    tech gig 2019 round 1 - new year party - score 100

--- src/main/java/ds/TechGigNewYarPartyMain.java
+++ src/main/java/ds/TechGigNewYarPartyMain.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 
 
 public class TechGigNewYarPartyMain {
@@ -25,7 +22,7 @@ public class TechGigNewYarPartyMain {
             if (i != 0) {
                 finalResultBuilder.append("\n");
             }
-            finalResultBuilder.append(techGigNewYearParty.findMaxSum().consolidateReverseWay());
+            finalResultBuilder.append(techGigNewYearParty.findMaxSum().consolidate());
         }
         System.out.println(finalResultBuilder.toString());
     }
@@ -33,30 +30,28 @@ public class TechGigNewYarPartyMain {
 }
 
 
+@SuppressWarnings("WeakerAccess")
 class TrailContainer implements Comparable {
     private int[] trail;
-    private int sum = 0;
-    private int currentLength = 0;
+    private int sum;
+    private int currentLength = 1;
 
     public TrailContainer(int maxTrailLength, int element) {
         trail = new int[maxTrailLength];
-        trail[0]=element;
-        this.sum=element;
+        trail[0] = element;
+        this.sum = element;
     }
 
     public TrailContainer addToTrail(int element) {
-        TrailContainer trailContainer=new TrailContainer(trail.length,0);
-        System.arraycopy(this.trail, 0, trailContainer.trail, 0, trail.length);
-        trailContainer.sum=this.sum+element;
-        trailContainer.currentLength = this.currentLength+1;
-        trailContainer.trail[this.currentLength+1]=element;
+        TrailContainer trailContainer = new TrailContainer(trail.length+1, 0);
+        for (int i=0;i<trail.length;i++)
+        {
+            trailContainer.trail[i]=this.elementAt(i);
+        }
+        trailContainer.sum = this.sum + element;
+        trailContainer.currentLength = this.currentLength + 1;
+        trailContainer.trail[this.currentLength + 1] = element;
         return trailContainer;
-    }
-
-    public void clearTrail() {
-        sum = 0;
-        currentLength = 0;
-        trail = new int[trail.length];
     }
 
     public int elementAt(int index) {
@@ -90,7 +85,7 @@ class TrailContainer implements Comparable {
         int trailLength = this.trail.length;
         int otherTrailLength = otherTrailContainer.trailLength();
         int shorterLength = trailLength >= otherTrailLength ? otherTrailLength : trailLength;
-        for (int i = shorterLength - 1; i >= 0; i--) {
+        for (int i = 0; i <shorterLength ; i++) {
             if (trail[i] != otherTrailContainer.elementAt(i)) {
                 return trail[i] - otherTrailContainer.elementAt(i);
             }
@@ -137,46 +132,39 @@ class TechGigNewYearParty {
 
         TrailContainer[] trailContainers = new TrailContainer[input.length];
         TrailContainer rightMostTrailContainer = new TrailContainer(input.length, input[input.length - 1]);
-        trailContainers[input.length-1] = rightMostTrailContainer;
+        trailContainers[input.length - 1] = rightMostTrailContainer;
 
-        int elementInSecondFromRightTrailContainer=0;
+        int elementInSecondFromRightTrailContainer;
         if (input[input.length - 2] > input[input.length - 1]) {
-            elementInSecondFromRightTrailContainer=(input[input.length - 2]);
+            elementInSecondFromRightTrailContainer = (input[input.length - 2]);
         } else {
-            elementInSecondFromRightTrailContainer=(input[input.length - 1]);
+            elementInSecondFromRightTrailContainer = (input[input.length - 1]);
         }
-        trailContainers[input.length-2] = new TrailContainer(input.length,elementInSecondFromRightTrailContainer);
+        trailContainers[input.length - 2] = new TrailContainer(input.length, elementInSecondFromRightTrailContainer);
 
         for (int i = input.length - 3; i >= 0; i--) {
-            int valueWithOneBeforeAdjacent = input[i] + trailContainers[i+2].getSum();
+            int valueWithOneBeforeAdjacent = input[i] + trailContainers[i + 2].getSum();
             Rearrange rearrange;
 
-            if (trailContainers[i + 2].getSum()>trailContainers[i + 1].getSum())
-            {
-                rearrange=Rearrange.OneBeforeAdjacent;
-            }else if(trailContainers[i + 2].getSum()<trailContainers[i + 1].getSum()){
-                rearrange=Rearrange.Adjacent;
-            }else{
-                rearrange = (trailContainers[i + 2].compareTo(trailContainers[i + 1])) < 0 ? Rearrange.Adjacent:Rearrange.OneBeforeAdjacent;
+            if (trailContainers[i + 2].getSum() > trailContainers[i + 1].getSum()) {
+                rearrange = Rearrange.OneBeforeAdjacent;
+            } else if (trailContainers[i + 2].getSum() < trailContainers[i + 1].getSum()) {
+                rearrange = Rearrange.Adjacent;
+            } else {
+                rearrange = (trailContainers[i + 2].compareTo(trailContainers[i + 1])) < 0 ? Rearrange.Adjacent : Rearrange.OneBeforeAdjacent;
             }
-            if (rearrange.getSum(input, i, trailContainers) < valueWithOneBeforeAdjacent)
-            {
-                rearrange=Rearrange.SelfWithOneBeforeAdjacent;
-            }else if (rearrange.getSum(input, i, trailContainers) == valueWithOneBeforeAdjacent)
-            {
-                if (rearrange.getTrailContainer(input, i, trailContainers).compareTo(Rearrange.SelfWithOneBeforeAdjacent.getTrailContainer(input, i, trailContainers)) <0 )
-                {
-                    rearrange=Rearrange.SelfWithOneBeforeAdjacent;
+            if (rearrange.getSum(input, i, trailContainers) < valueWithOneBeforeAdjacent) {
+                rearrange = Rearrange.SelfWithOneBeforeAdjacent;
+            } else if (rearrange.getSum(input, i, trailContainers) == valueWithOneBeforeAdjacent) {
+                if (rearrange.getTrailContainer(input, i, trailContainers).compareTo(Rearrange.SelfWithOneBeforeAdjacent.getTrailContainer(input, i, trailContainers)) < 0) {
+                    rearrange = Rearrange.SelfWithOneBeforeAdjacent;
                 }
             }
-            if (rearrange.getSum(input, i, trailContainers) < input[i])
-            {
-                rearrange=Rearrange.Self;
-            }else if (rearrange.getSum(input, i, trailContainers) == input[i])
-            {
-                if (rearrange.getTrailContainer(input, i, trailContainers).compareTo(Rearrange.Self.getTrailContainer(input, i, trailContainers)) <0 )
-                {
-                    rearrange=Rearrange.Self;
+            if (rearrange.getSum(input, i, trailContainers) < input[i]) {
+                rearrange = Rearrange.Self;
+            } else if (rearrange.getSum(input, i, trailContainers) == input[i]) {
+                if (rearrange.getTrailContainer(input, i, trailContainers).compareTo(Rearrange.Self.getTrailContainer(input, i, trailContainers)) < 0) {
+                    rearrange = Rearrange.Self;
                 }
             }
             rearrange.updateTrailForElement(input, i, trailContainers);
@@ -186,113 +174,89 @@ class TechGigNewYearParty {
 
 }
 
-enum Rearrange
-{
-    Self
-            {
-                @Override
-                public void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    TrailContainer trailContainer=new TrailContainer(input.length, input[elementPosition]);
-                    trailContainers[elementPosition] = trailContainer;
-                }
+enum Rearrange {
+    Self {
+        @Override
+        public void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            TrailContainer trailContainer = new TrailContainer(input.length, input[elementPosition]);
+            trailContainers[elementPosition] = trailContainer;
+        }
 
-                @Override
-                public TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    return new TrailContainer(1, input[elementPosition]);
-                }
+        @Override
+        public TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            return new TrailContainer(1, input[elementPosition]);
+        }
 
-                @Override
-                public int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    return input[elementPosition];
-                }
-            },
-    SelfWithOneBeforeAdjacent
-            {
-                @Override
-                public void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    trailContainers[elementPosition]=trailContainers[elementPosition+2].addToTrail(input[elementPosition]);
-                }
+        @Override
+        public int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            return input[elementPosition];
+        }
+    },
+    SelfWithOneBeforeAdjacent {
+        @Override
+        public void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            trailContainers[elementPosition] = trailContainers[elementPosition + 2].addToTrail(input[elementPosition]);
+        }
 
-                @Override
-                public TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    return trailContainers[elementPosition + 2];
-                }
+        @Override
+        public TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            return trailContainers[elementPosition + 2];
+        }
 
-                @Override
-                public int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    return input[elementPosition]+trailContainers[elementPosition + 2].getSum();
-                }
-            },
-    Adjacent
-            {
-                @Override
-                public void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    trailContainers[elementPosition]=trailContainers[elementPosition+1];
-                }
+        @Override
+        public int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            return input[elementPosition] + trailContainers[elementPosition + 2].getSum();
+        }
+    },
+    Adjacent {
+        @Override
+        public void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            trailContainers[elementPosition] = trailContainers[elementPosition + 1];
+        }
 
-                @Override
-                public TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    return trailContainers[elementPosition + 1];
-                }
+        @Override
+        public TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            return trailContainers[elementPosition + 1];
+        }
 
-                @Override
-                public int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    return trailContainers[elementPosition + 1].getSum();
-                }
-            },
-    OneBeforeAdjacent
-            {
-                @Override
-                public void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    trailContainers[elementPosition]=trailContainers[elementPosition+2];
-                }
+        @Override
+        public int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            return trailContainers[elementPosition + 1].getSum();
+        }
+    },
+    OneBeforeAdjacent {
+        @Override
+        public void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            trailContainers[elementPosition] = trailContainers[elementPosition + 2];
+        }
 
-                @Override
-                public TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    return trailContainers[elementPosition + 2];
-                }
+        @Override
+        public TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            return trailContainers[elementPosition + 2];
+        }
 
-                @Override
-                public int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers) {
-                    return trailContainers[elementPosition + 2].getSum();
-                }
-            };
+        @Override
+        public int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers) {
+            return trailContainers[elementPosition + 2].getSum();
+        }
+    };
 
     public abstract void updateTrailForElement(int[] input, int elementPosition, TrailContainer[] trailContainers);
+
     public abstract TrailContainer getTrailContainer(int[] input, int elementPosition, TrailContainer[] trailContainers);
-    public abstract int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers );
+
+    public abstract int getSum(int[] input, int elementPosition, TrailContainer[] trailContainers);
 }
 
 @SuppressWarnings("WeakerAccess")
 class Result {
-    private List<Integer> trail;
     private TrailContainer trailContainer;
 
     public Result(TrailContainer trailContainer) {
         this.trailContainer = trailContainer;
     }
 
-    public Result(List<Integer> trail) {
-        this.trail = trail;
-    }
-
-    public String consolidateReverseWay() {
-
-
-        StringBuilder resultBuffer = new StringBuilder();
-
-        if (trailContainer != null) {
-            return trailContainer.toString();
-        } else {
-
-
-            Collections.reverse(trail);
-            trail.forEach(integer -> {
-                if (!integer.equals(0)) {
-                    resultBuffer.append(integer);
-                }
-            });
-        }
-        return resultBuffer.toString();
+    public String consolidate() {
+        return trailContainer.toString();
     }
 }
--- src/test/java/ds/TechGigNewYearPartyTest.java
+++ src/test/java/ds/TechGigNewYearPartyTest.java
@@ -2,14 +2,15 @@ package ds;
 
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.IntConsumer;
+import java.util.Random;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class TechGigNewYearPartyTest {
 
@@ -17,140 +18,157 @@ public class TechGigNewYearPartyTest {
     public void testCase1() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {6,5,1,-9,-8,4,-1,11});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("11416") );
+        assertThat(maxSum.consolidate(), is("11416") );
     }
 
     @Test
     public void testCase2() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {-1,7,8,-5,4});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("48") );
+        assertThat(maxSum.consolidate(), is("48") );
     }
 
     @Test
     public void testCase3() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {11,12,-2,-1});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("12") );
+        assertThat(maxSum.consolidate(), is("12") );
     }
 
     @Test
     public void testCase4() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {4,3,4,5});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("54") );
+        assertThat(maxSum.consolidate(), is("54") );
     }
 
     @Test
     public void testCase5() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {5, 10, 4, -1});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("10") );
+        assertThat(maxSum.consolidate(), is("10") );
     }
 
     @Test
     public void testCase6() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {-1, -1, 10, 1});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("10") );
+        assertThat(maxSum.consolidate(), is("10") );
     }
 
     @Test
     public void testCase7() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {6, 9, -3, 9, 9, 2, 4, 8, -7, -5, 9});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("98299") );
+        assertThat(maxSum.consolidate(), is("98299") );
     }
 
     @Test
     public void testCase8() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {9,-9});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("9") );
+        assertThat(maxSum.consolidate(), is("9") );
     }
 
     @Test
     public void testCase9() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {1,2,4,0,0,0,7,9,0,0,0,0,-1,-2,-3,100});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("100941") );
+        assertThat(maxSum.consolidate(), is("100941") );
     }
 
     @Test
     public void testCase10() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {4, 5, 4, 3, -2, -2, -2, 4, 5, 4, 3});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("4444") );
+        assertThat(maxSum.consolidate(), is("4444") );
     }
 
     @Test
     public void testCase11() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {9,1,2,8});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("89") );
+        assertThat(maxSum.consolidate(), is("89") );
     }
 
     @Test
     public void testCase12() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {-1,-2,-3,-4});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("-1") );
+        assertThat(maxSum.consolidate(), is("-1") );
     }
 
     @Test
     public void testCase13() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {2,4,6,4});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("62") );
+        assertThat(maxSum.consolidate(), is("62") );
     }
 
     @Test
     public void testCase14() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {4,6,4,2});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("44") );
+        assertThat(maxSum.consolidate(), is("44") );
     }
 
     @Test
     public void testCase15() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {5,6,3,2});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("35") );
+        assertThat(maxSum.consolidate(), is("35") );
     }
 
     @Test
     public void testCase16() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {5,5,4,4});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("45") );
+        assertThat(maxSum.consolidate(), is("45") );
     }
 
     @Test
     public void testCase17() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {6,0,0,2});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("26") );
+        assertThat(maxSum.consolidate(), is("26") );
     }
 
     @Test
     public void testCase18() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {5,4,2,4,1});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("45") );
+        assertThat(maxSum.consolidate(), is("45") );
     }
 
     @Test
     public void testCase19() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {1,4,2,4,5});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("54") );
+        assertThat(maxSum.consolidate(), is("54") );
     }
 
     @Test
     public void testCase20() {
         TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(new int[] {-1,-1,0,-1});
         Result maxSum = techGigNewYearParty.findMaxSum();
-        assertThat(maxSum.consolidateReverseWay(), is("") );
+        assertThat(maxSum.consolidate(), is("") );
+    }
+
+    @Test
+    public void testWithTime1()
+    {
+        for (int j=0;j<200;j++) {
+            int[] testCase = new int[1000];
+            Random random = new Random();
+            for (int i = 0; i < testCase.length; i++) {
+                testCase[i] = random.nextInt();
+            }
+            TechGigNewYearParty techGigNewYearParty = new TechGigNewYearParty(testCase);
+            ZonedDateTime start = ZonedDateTime.now();
+            techGigNewYearParty.findMaxSum().consolidate();
+            ZonedDateTime end= ZonedDateTime.now();
+            System.out.println(Duration.between(start, end).toMillis());
+        }
     }
 
     @Test
@@ -172,19 +190,16 @@ public class TechGigNewYearPartyTest {
         }};
 
 
-        expectedTestCases.forEach(new Consumer<String>() {
-            @Override
-            public void accept(String s) {
-                String input = s.split(",")[0];
-                String output = s.split(",")[1];
+        expectedTestCases.forEach(s -> {
+            String input = s.split(",")[0];
+            String output = s.split(",")[1];
 
 
-                TechGigNewYearParty techGigNewYearParty=new TechGigNewYearParty(Arrays.stream(input.split(" "))
-                        .mapToInt(Integer::parseInt)
-                        .toArray());
-                assertThat(techGigNewYearParty.findMaxSum().consolidateReverseWay(), is(output));
+            TechGigNewYearParty techGigNewYearParty=new TechGigNewYearParty(Arrays.stream(input.split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray());
+            assertThat(techGigNewYearParty.findMaxSum().consolidate(), is(output));
 
-            }
         });
 
     }
