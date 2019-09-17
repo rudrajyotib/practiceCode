67e2bb10a2cd0ab68076a088f81e2577f5e2cc62
607112663 <rudrajyoti.biswas@bt.com>
2019-09-17 05:30:00 +0100

    Leet code - hard - self crossing - refactor

--- src/main/java/leetcode/hard/selfCrossing/Solution.java
+++ src/main/java/leetcode/hard/selfCrossing/Solution.java
@@ -1,5 +1,8 @@
 package leetcode.hard.selfCrossing;
 
+/*
+https://leetcode.com/problems/self-crossing/
+ */
 public class Solution
 {
     private static boolean fallsInBetween(int number, int startRange, int endRange)
@@ -34,13 +37,6 @@ public class Solution
             return false;
         }
         MovementTracker movementTracker = new MovementTracker();
-        /*for (int i : x)
-        {
-            if (movementTracker.checkIfCrossAndUpdate(i))
-            {
-                return true;
-            }
-        }*/
         return movementTracker.check(x);
     }
 
@@ -51,23 +47,22 @@ public class Solution
         int movementCounter = 0;
         String[] directions = new String[]{"North", "West", "South", "East"};
         boolean outbound = true;
-        PointDefinition[] corners = new PointDefinition[]{new PointDefinition(0, 0), null, null, null};
         BoundaryDefinition[] boundaryDefinitions = new BoundaryDefinition[]{new BoundaryDefinition(), new BoundaryDefinition(), new BoundaryDefinition(), new BoundaryDefinition()};
 
         boolean check(int[] steps)
         {
-            corners[1] = new PointDefinition(0, steps[0]);
-            corners[2] = new PointDefinition(0 - steps[1], steps[0]);
-            corners[3] = new PointDefinition(0 - steps[1], steps[0] - steps[2]);
 
-            boundaryDefinitions[0].update(corners[0].x, corners[0].y, corners[1].x, corners[1].y);
-            boundaryDefinitions[1].update(corners[1].x, corners[1].y, corners[2].x, corners[2].y);
-            boundaryDefinitions[2].update(corners[2].x, corners[2].y, corners[3].x, corners[3].y);
+            int leftBottomX = 0 - steps[1];
+            int leftBottomY = steps[0] - steps[2];
 
-            if (corners[3].y >= 0)
+            boundaryDefinitions[0].update(0, 0, 0, steps[0]);
+            boundaryDefinitions[1].update(0, steps[0], 0 - steps[1], steps[0]);
+            boundaryDefinitions[2].update(0 - steps[1], steps[0], 0 - steps[1], steps[0] - steps[2]);
+
+            if (leftBottomY >= 0)
             {
                 outbound = false;
-                if (boundaryDefinitions[0].doesLineCross(corners[3].x, corners[3].y, corners[3].x + steps[3], corners[3].y))
+                if (boundaryDefinitions[0].doesLineCross(leftBottomX, leftBottomY, leftBottomX + steps[3], leftBottomY))
                 {
                     return true;
                 }
@@ -78,18 +73,18 @@ public class Solution
                 return false;
             }
 
-            else if (fallsInBetween(corners[3].x + steps[3], boundaryDefinitions[1].startX, boundaryDefinitions[1].endX))
+            else if (fallsInBetween(leftBottomX + steps[3], boundaryDefinitions[1].startX, boundaryDefinitions[1].endX))
             {
                 outbound = false;
             }
-            if (corners[3].x + steps[3] == 0)
+            if (leftBottomX + steps[3] == 0)
             {
-                boundaryDefinitions[1].update(0, 0, corners[3].x, corners[3].y);
+                boundaryDefinitions[1].update(0, 0, leftBottomX, leftBottomY);
             }
 
-            boundaryDefinitions[3].update(corners[3].x, corners[3].y, corners[3].x + steps[3], corners[3].y);
-            currentX = corners[3].x + steps[3];
-            currentY = corners[3].y;
+            boundaryDefinitions[3].update(leftBottomX, leftBottomY, leftBottomX + steps[3], leftBottomY);
+            currentX = leftBottomX + steps[3];
+            currentY = leftBottomY;
 
             for (int i = 4; i < steps.length; i++)
             {
@@ -225,18 +220,6 @@ public class Solution
             ++movementCounter;
 
             return false;
-        }
-    }
-
-    private static class PointDefinition
-    {
-        int x;
-        int y;
-
-        PointDefinition(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
         }
     }
 
